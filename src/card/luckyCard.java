b3f775ee657ac428cdf26d36a4e13ac8d2cb63a2
Khushbu09A <khushbuk@sheridancollege.ca>
2023-05-31 16:53:41 -0400

    Student project version7

---
+++ src/card/luckyCard.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package card;
+
+/**
+ *
+ * @author ks343
+ */
+class luckyCard {
+
+    static int getValue() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Object getSuit() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
